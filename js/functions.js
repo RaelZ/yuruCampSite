bb02f78e18eeac2caf6cd8e426ba32e4e1157d6c
Israel Ferreira <48022462+RaelZ@users.noreply.github.com>
2021-03-19 18:33:52 -0300

    Delete temporário
    Arquivo JS foi movido para main temporariamente.

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-function mobile() {
-    var menu = document.getElementById("menu-mobile");
-    if (menu.style.display === "none") {
-        menu.style.display = "inline-block";
-    } else {
-        menu.style.display = "none";
-    }
-}
